3b2702355381772b2332498155323092dc40fa2e
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-02-19 11:42:40 -0300

    Aula 0

--- app/page.tsx
+++ app/page.tsx
@@ -24,8 +24,8 @@ export default function Home() {
               height={24}
               priority
             />
-          </a>
-        </div>
+          </a>          
+        </div>        
       </div>
 
       <div className={styles.center}>
@@ -37,6 +37,7 @@ export default function Home() {
           height={37}
           priority
         />
+        <h1>Aula next</h1>
       </div>
 
       <div className={styles.grid}>
