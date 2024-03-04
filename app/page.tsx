dd93416370df7cbebfcf6d551f27f3064bc58b1b
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-03-04 09:49:10 -0300

    aula css

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return(
+    <>
+      <h1>Hello, Next.js!</h1>
+      <h2>Título 2</h2>
+      <h3>Título 3</h3>
+    </>
+  );
 }
