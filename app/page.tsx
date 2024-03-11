6323e9e385980369a9081385933ecbd241c33d35
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-03-11 09:48:48 -0300

    aula 1 - 11-03

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Page() {
   return(
     <>
@@ -5,6 +7,9 @@ export default function Page() {
       <h2>Título 2</h2>
       <h3>Título 3</h3> 
       Qualquer coisa
+
+    <Link href={'/sobre'}>Sobre</Link>
+    <Link href={'/cadusuario'}>Cadastro de usuário</Link>
     </>
   );
 }
