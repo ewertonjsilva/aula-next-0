c092fdb8103f3488820c647e90ebaa53e8b55e7e
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-03-18 11:40:00 -0300

    aula 18-03

---
+++ src/usuarios.js
@@ -0,0 +1,20 @@
+const usuarios = [
+    {
+        id: 1, 
+        nome: 'Jamildo James', 
+        altura: 1.96, 
+        ativo: true
+    }, 
+    {
+        id: 2, 
+        nome: 'Eliza Larissa', 
+        altura: 1.61, 
+        ativo: false
+    }, 
+    {
+        id: 3, 
+        nome: 'Cléston Carvalho', 
+        altura: 1.72, 
+        ativo: true
+    }, 
+];
