db2c7926e725cb16a8e176a470688dc903b97089
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-03-25 11:46:33 -0300

    aula 25-03

--- app/usuarios/[id]/page.tsx
+++ app/usuarios/[id]/page.tsx
@@ -1,10 +1,19 @@
 import Link from 'next/link';
 
-export default function PerfilUsuario ({ params }: { params: { id: string } }) {
+import usuarios from '@/src/usuarios';
+
+export default function PerfilUsuario ({ params }: { params: { id: string } }) { 
+
+    const usuario = usuarios.filter((usu, index, array) => usu.id === parseInt(params.id));
+
     return(
         <>
             <h1>Perfil</h1>
-            <p>Nome do usuário {params.id}</p>
+            <div>
+                <p>Nome: {usuario[0].nome}</p>
+                <p>Altura: {usuario[0].altura}</p>
+                <p>Ativo: {usuario[0].ativo ? 'Sim' : 'Não'}</p>
+            </div>
 
             <p><Link href={'/usuarios'}>Voltar</Link></p>            
         </>
--- app/usuarios/page.tsx
+++ app/usuarios/page.tsx
@@ -1,25 +1,34 @@
-import Link from 'next/link'; 
+import Link from 'next/link';
 
 import usuarios from '../../src/usuarios';
 
 
-export default function ListaUsuarios () {
+export default function ListaUsuarios() {
 
     // console.log(usuarios);
     // console.log(usuarios[0].nome);
-    
-    return(
+
+    return (
         <>
             <h1>Usuários</h1>
-            {/* <p><Link href={'/usuarios/1'}>Usuário 1</Link></p>
-            <p><Link href={'/usuarios/2'}>Usuário 2</Link></p>
-            <p><Link href={'/usuarios/3'}>Usuário 3</Link></p> */}
+            {/* 
+                <p><Link href={'/usuarios/1'}>Usuário 1</Link></p>
+                <p><Link href={'/usuarios/2'}>Usuário 2</Link></p>
+                <p><Link href={'/usuarios/3'}>Usuário 3</Link></p> 
+            */}
+
+            {/*
+                usuarios.map((usuario) => <p>Nome: {usuario.nome}</p>)
+            */}
 
             {
-                usuarios.map((usuario) => <p>Nome: {usuario.nome}</p>)
+                usuarios.map((usuario) => <p>
+                    <Link href={`/usuarios/${usuario.id}`}>{usuario.nome}</Link>
+                </p>)
             }
 
-            <p><Link href={'/'}>Voltar</Link></p>            
+
+            <p><Link href={'/'}>Voltar</Link></p>
         </>
     );
 }
